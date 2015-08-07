4a97b3efd11589f7a31fe0fa6454bbfcf9ecbf64
shaung <shaun.geng@gmail.com>
2015-08-07 21:26:18 +0900

    Fix #7: Unicode error on non-ascii content.

--- autosub/formatters.py
+++ autosub/formatters.py
@@ -1,11 +1,23 @@
+# -*- coding: utf-8 -*-
+import sys
 import json
+
 import pysrt
+
+text_type = unicode if sys.version_info < (3,) else str
+
+
+def force_unicode(s, encoding="utf-8"):
+    if isinstance(s, text_type):
+        return s
+    return s.decode(encoding)
+
 
 def srt_formatter(subtitles, show_before=0, show_after=0):
     f = pysrt.SubRipFile()
     for (rng, text) in subtitles:
         item = pysrt.SubRipItem()
-        item.text = text
+        item.text = force_unicode(text)
         start, end = rng
         item.start.seconds = max(0, start - show_before)
         item.end.seconds = end + show_after
