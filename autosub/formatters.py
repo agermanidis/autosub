8464f6558d61666826656f40b2f2f8c7cd8a76f6
antonio machado <machado19@gmail.com>
2016-11-04 00:21:24 -0200

    including vtt subtitles support

--- autosub/formatters.py
+++ autosub/formatters.py
@@ -24,6 +24,18 @@ def srt_formatter(subtitles, show_before=0, show_after=0):
         item.end.seconds = end + show_after
         f.append(item)
     return '\n'.join(map(unicode, f))
+
+def vtt_formatter(subtitles, show_before=0, show_after=0):
+    f = pysrt.SubRipFile()
+    for i, (rng, text) in enumerate(subtitles, 1):
+        item = pysrt.SubRipItem()
+        item.index = i
+        item.text = force_unicode(text)
+        start, end = rng
+        item.start.seconds = max(0, start - show_before)
+        item.end.seconds = end + show_after
+        f.append(item)
+    return '\n'.join(map(unicode, f))
         
 def json_formatter(subtitles):
     subtitle_dicts = map(lambda (r, t): {'start': r[0], 'end': r[1], 'content': t}, subtitles)
@@ -34,6 +46,7 @@ def raw_formatter(subtitles):
 
 FORMATTERS = {
     'srt': srt_formatter,
+    'vtt': vtt_formatter,
     'json': json_formatter,
     'raw': raw_formatter,
 }
