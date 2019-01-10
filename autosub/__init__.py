6a1f712b66f6d817d7c99ee43b9c00a92df7692c
Wang Jiaxiang <wangjaixiang96@outlook.com>
2019-01-10 14:33:18 +0800

    Fix "ValueError" when the response data of "SpeechRecgnizer"  coundn't be parsed to JSON Object.

--- autosub/__init__.py
+++ autosub/__init__.py
@@ -99,7 +99,7 @@ class SpeechRecognizer(object): # pylint: disable=too-few-public-methods
                         line = json.loads(line)
                         line = line['result'][0]['alternative'][0]['transcript']
                         return line[:1].upper() + line[1:]
-                    except IndexError:
+                    except (ValueError, IndexError):                        
                         # no result
                         continue
 
