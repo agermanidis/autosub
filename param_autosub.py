018ada9864706629c46a6a395953b1e159939a50
Phra Adhipañño <adhipanno.bhikkhu@gmail.com>
2019-01-25 18:18:41 +0700

    Added License notes

--- param_autosub.py
+++ param_autosub.py
@@ -12,16 +12,9 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-import platform
-import os
-import subprocess
+class Param_Autosub():
 
-class MyUtil(object):
-    @staticmethod
-    def open_file(path):
-        if platform.system() == "Windows":
-            os.startfile(path)
-        elif platform.system() == "Darwin":
-            subprocess.Popen(["open", path])
-        else:
-            subprocess.Popen(["xdg-open", path])
+    def __init__(self, listFiles, outputFolder, langCode):
+        self.listFiles = listFiles
+        self.outputFolder = outputFolder
+        self.langCode = langCode
