905447323894615dfe04b439379012a4f4eb4b22
Phra Adhipañño <adhipanno.bhikkhu@gmail.com>
2019-01-26 04:52:20 +0700

    Added License header

--- srtparser.py
+++ srtparser.py
@@ -1,4 +1,16 @@
-#based on https://gist.github.com/ndunn219/62263ce1fb59fda08656be7369ce329b
+'''
+   (C) 2019 Raryel C. Souza
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import re, sys
 
