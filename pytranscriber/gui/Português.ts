d94e9fc0bd1104e062ed4e9bfa0ad84b5429ffa7
Raryel <raryel.costa@gmail.com>
2022-08-17 19:02:31 -0300

    Translation files for Portuguese, Chinese Traditional and Chinese Simplified

---
+++ pytranscriber/gui/Português.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>window</name>
+    <message>
+        <location filename="gui.py" line="129"/>
+        <source>pyTranscriber - v1.8 - 17/08/2022</source>
+        <translation>pyTranscriber -v1.8 - 17/08/2022</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="130"/>
+        <source>Select file(s)</source>
+        <translation>Selecionar arquivo(s)</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="131"/>
+        <source>Transcribe Audio / Generate Subtitles</source>
+        <translation>Transcrever áudio / Gerar Legendas</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="132"/>
+        <source>Open Output Folder</source>
+        <translation>Abrir Pasta de Destino</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="133"/>
+        <source>Output Location</source>
+        <translation>Pasta de Destino</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="134"/>
+        <source>List of files to generate transcribe audio / generate subtitles</source>
+        <translation>Lista de arquivos para gerar legendas/transcrever áudio</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="135"/>
+        <source>Remove file(s)</source>
+        <translation>Remover arquivo(s)</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="136"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="137"/>
+        <source>Open output files automatically</source>
+        <translation>Abrir arquivos de saída automaticamente</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="138"/>
+        <source>Audio Language:</source>
+        <translation>Idioma do áudio:</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="139"/>
+        <source>Abo&amp;ut</source>
+        <translation>Sob&amp;re</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="140"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Configurações</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="141"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Idioma</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="142"/>
+        <source>&amp;License</source>
+        <translation>&amp;Licença</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="143"/>
+        <source>&amp;Funding at Github Sponsors</source>
+        <translation>Patrocínio no GitHub Sponsors</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="144"/>
+        <source>&amp;About pyTranscriber</source>
+        <translation>&amp;Sobre o pyTranscriber</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="145"/>
+        <source>&amp;Proxy</source>
+        <translation>Proxy</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="146"/>
+        <source>Proxy setting</source>
+        <translation>Configurações de Proxy</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="147"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="148"/>
+        <source>繁體中文 - Chinese Traditional</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="149"/>
+        <source>简体中文 - Chinese Simplified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="150"/>
+        <source>Português</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ pytranscriber/gui/简体中文 - Chinese Simplified.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh" sourcelanguage="en">
+<context>
+    <name>window</name>
+    <message>
+        <location filename="gui.py" line="114"/>
+        <source>pyTranscriber - v1.7 - 08/08/2020</source>
+        <translation>pyTranscriber -v1.8 - 20/08/2022</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="115"/>
+        <source>Select file(s)</source>
+        <translation>选择文件</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="116"/>
+        <source>Transcribe Audio / Generate Subtitles</source>
+        <translation>转译音频 / 生成字幕</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="117"/>
+        <source>Open Output Folder</source>
+        <translation>打开导出文件夹</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="118"/>
+        <source>Output Location</source>
+        <translation>导出位置</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="119"/>
+        <source>&amp;List of files to generate transcribe audio / generate subtitles</source>
+        <translation>&amp;转译 / 生成字幕文件列表</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="120"/>
+        <source>Remove file(s)</source>
+        <translation>移除文件</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="121"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="122"/>
+        <source>Open output files automatically</source>
+        <translation>完成后自动打开文件夹</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="123"/>
+        <source>Audio Language:</source>
+        <translation>选择音频语言</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="124"/>
+        <source>Abo&amp;ut</source>
+        <translation>Abo&amp;ut</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="125"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="141"/>
+        <source>&amp;Language</source>
+        <translation>语</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="126"/>
+        <source>&amp;License</source>
+        <translation>&amp;License</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="127"/>
+        <source>&amp;Funding at Github Sponsors</source>
+        <translation>资助 GitHub 上的项目</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="128"/>
+        <source>&amp;About pyTranscriber</source>
+        <translation>&amp;关于 pyTranscriber</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="129"/>
+        <source>Proxy</source>
+        <translation>Proxy</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="130"/>
+        <source>Proxy settings</source>
+        <translation>Proxy settings</translation>
+    </message>
+</context>
+</TS>
