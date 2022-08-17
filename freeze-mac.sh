7fbc7e97405ecb194234ef86fa50f94f653d2696
Raryel <raryel.costa@gmail.com>
2022-08-17 19:51:20 -0300

    Updated build script mac include lang files

--- freeze-mac.sh
+++ freeze-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pipenv shell
-pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --clean --windowed
+pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" --clean --windowed
