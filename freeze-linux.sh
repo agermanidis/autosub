1c633f7d0266ac3d4ce08dc9f093495599989141
Raryel <raryel.costa@gmail.com>
2022-08-17 19:43:20 -0300

    lang files added to build script

--- freeze-linux.sh
+++ freeze-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pipenv shell
-pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --onefile --clean
+pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" --onefile --clean
